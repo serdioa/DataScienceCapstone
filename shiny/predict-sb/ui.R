a0262e9c8bcff916b27fd929bd4e0e0c89924b51
serdioa <serdioa@t-online.de>
2019-07-25 12:12:23 +0200

    Cleaned up Shiny application

--- shiny/predict-sb/ui.R
+++ shiny/predict-sb/ui.R
@@ -18,66 +18,40 @@ shinyUI(
         tabPanel(
             "Prediction",
             fluidPage(
-                fluidRow(column(
-                    12,
-                    p(
-                        "Start typing text in the input area below. The prediction of the next word will appear and update as you type."
-                    )
+                fluidRow(column(12,
+                    p("Start typing text in the input area below. The prediction of the next word will appear and update as you type.")
                 )),
-                fluidRow(column(
-                    12,
-                    p(
-                        "If the checkbox 'Predict partially entered words' is selected, the application will attempt to predict continuation of a word you are currently typing. In this mode, ", tags$strong("the next word is predicted only after you type the space character"), " to indicate that you have finished entering the previous word."
-                    )
+                fluidRow(column(12,
+                    p("If the checkbox 'Predict partially entered words' is selected, the application will attempt to predict continuation of a word you are currently typing. In this mode, ", tags$strong("the next word is predicted only after you type the space character"), " to indicate that you have finished entering the previous word.")
                 )),
-                fluidRow(column(
-                    12,
-                    p(
-                        "When predicting partially entered words, if you start entering a word unknown to the application, it assumes the word to be misspelled and suggests a correction. Try to enter 'mashine' instead of 'machine'."
-                    )
+                fluidRow(column(12,
+                    p("When predicting partially entered words, if you start entering a word unknown to the application, it assumes the word to be misspelled and suggests a correction. Try to enter 'mashine' instead of 'machine'.")
                 )),
-                fluidRow(column(
-                    12,
-                    p(
-                        "Use the slider below the input area to choose the number of predicted words. Click the button 'Random Sample' to fill the input area with one of over 1000 prepared sample texts."
-                    )
+                fluidRow(column(12,
+                    p("Use the slider below the input area to choose the number of predicted words. Click the button 'Random Sample' to fill the input area with one of over 1000 prepared sample texts.")
                 )),
-                fluidRow(column(
-                    12,
-                    p(
-                        "Choose the tab 'About' in the headline to learn more about this application."
-                    )
+                fluidRow(column(12,
+                    p("Choose the tab 'About' in the headline to learn more about this application.")
                 )),
-                fluidRow(column(
-                    12,
+                fluidRow(column(12,
                     textAreaInput("textPrefix", label = NULL, width = "800px")
                 )),
-                fluidRow(column(
-                    4,
-                    sliderInput(
-                        "n.predictions",
-                        "Number of predictions:",
-                        min = 1,
-                        max = 10,
-                        value = 5
-                    )
+                fluidRow(column(4,
+                    sliderInput("n.predictions", "Number of predictions:",
+                                min = 1, max = 10, value = 5)
                 ),
-                column(
-                    2,
-                    offset = 2,
-                    actionButton("randomSample",
-                                 label = "Random Sample")
+                column(2, offset = 2,
+                    actionButton("randomSample", label = "Random Sample")
                 )),
-                fluidRow(column(
-                    12,
+                fluidRow(column(12,
                     checkboxInput("predictPartial",
                                   label = "Predict partially entered words",
                                   value = TRUE)
-                ))
-                ,
-                fluidRow(column(12,
-                                plotlyOutput("predictionPlot")))
+                )),
+                fluidRow(column(12, plotlyOutput("predictionPlot")))
             )
         ),
-        tabPanel("About")
+        tabPanel("About",
+                 column(12, includeMarkdown("about.md"))
+    )
 ))
